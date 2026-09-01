dc436b33401124d15578fae39b739d66c6458f3c
Vadym Delegach <vdelegach@gmail.com>
2026-09-01 19:07:42 +0300

    oxiXesp0.1.2 (01/09/2026) Exchange with STM32 baudrate - 9600

--- main/main.c
+++ main/main.c
@@ -24,7 +24,7 @@ void app_main(void)
     /* Configure parameters of an UART driver,
      * communication pins and install the driver */
     uart_config_t uart_config = {
-        .baud_rate = 115200,
+        .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
